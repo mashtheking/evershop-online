1dc65ac18d99c4a489dba4899d118994b6901371
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-08-14 22:38:47 +0300

    Evershop online UI project setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# EverShop Online
+
+### Colors code used:
+
+```sh
+#fcfcfc
+#768048
+#8d9968
+#a4b288
+#bbcba9
+```
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-center py-5">Ever Shop Online</h1>
+    </>
+  );
+}
+
+export default App;
